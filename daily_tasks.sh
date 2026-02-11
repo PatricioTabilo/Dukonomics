c6e85ffa02f6a35e069f187b2446a492b31ed291
Patricio Tabilo <pj.tabilo@gmail.com>
2026-02-11 11:57:44 -0600

    chore(automation): remove deprecated daily_tasks.sh

--- daily_tasks.sh
+++
@@ -1,5 +0,0 @@
-# Deprecated task queue format.
-# The automation now uses: .github/automation/daily_tasks.jsonl
-#
-# Each task is one JSON object line with required fields:
-# id, file, commit, instruction
